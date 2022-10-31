dffc617f18bd714c4ce9d79779a686bc0b84f815
Naimcoder <isteiaknaim12@gmail.com>
2022-10-31 16:52:54 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const[users,setUsers]=useState([])
+
+  useEffect(()=>{
+  fetch('http://localhost:5000/users')
+  .then(res=>res.json())
+  .then(data=>setUsers(data))
+  },[])
+
+const handleSubmit=(event)=>{
+event.preventDefault()
+const name= event.target.name.value;
+const email= event.target.email.value;
+const user= {name,email}
+fetch('http://localhost:5000/users',{
+   method:'POST',
+   headers:{'Content-Type':'application/json'},
+   body: JSON.stringify(user)
+})
+.then(res=>res.json())
+.then(data=>{
+  console.log(data)
+  const newData= [...users,data]
+  setUsers(newData)
+})
+event.target.reset()
+console.log(user)
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <form onSubmit = {handleSubmit}>
+         <input type="text"  name='name'id='name' placeholder='Name' />
+         <br></br>
+         <input type="email"  name='email'id='email' placeholder='Email' />
+         <br/>
+         <button type='submit'>Add user</button>
+      </form>
+
+      <h2>User:{users.length}</h2>
+      {users.map(user=><p key={user._id}>{user.name}{user.email}</p>)}
     </div>
   );
 }
